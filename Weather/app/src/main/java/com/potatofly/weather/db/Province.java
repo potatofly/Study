340313fb163310fe271005a21f49a5af91669ddd
liangguo.ji <liangguo.ji@jrdcom.com>
2017-01-09 18:50:59 +0800

    Weather ok

---
+++ Weather/app/src/main/java/com/potatofly/weather/db/Province.java
@@ -0,0 +1,37 @@
+package com.potatofly.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by potatofly on 1/8/17.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
---
+++ Weather/app/src/main/java/com/potatofly/weather/gson/AQI.java
@@ -0,0 +1,18 @@
+package com.potatofly.weather.gson;
+
+/**
+ * Created by potatofly on 1/8/17.
+ */
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+    }
+
+}
